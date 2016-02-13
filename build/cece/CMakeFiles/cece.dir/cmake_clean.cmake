faeaecc3a817807a361ad698a7b5415668f4e356
GustavoPB <g.puig.baeza@gmail.com>
2016-02-13 21:13:03 +0100

    -Se elemina carpeta Build del repositorio

--- build/cece/CMakeFiles/cece.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libcece.pdb"
-  "libcece.dylib"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/cece.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/cece/plugins/cell-python/CMakeFiles/plugin-cell-python.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for plugin-cell-python.
-# This may be replaced when dependencies are built.
